91b6566273009ab4c5c28bed7d102615742c66cc
Tony Skinner <tonyskinner@masabi.com>
2019-02-27 12:48:25 +0000

    Getting the name of the pokemon from the API response

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -8,3 +8,23 @@
 #
 # for j in search(query, tld="co.uk",start=0,stop=10):
 #     print(j)
+
+import requests
+import json
+
+r = requests.get('https://pokeapi.co/api/v2/ability/51/')
+
+resultContent = json.loads(r.content)
+
+# resultContent = resultContent['pokemon']
+
+resultContent = resultContent['pokemon']
+
+lastResult = resultContent[len(resultContent) -1]
+
+print lastResult
+
+print lastResult['pokemon']['name']
+
+# text = "bill,bob,ben"
+# print text.split(",")[1]
