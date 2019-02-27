62543d43941779312f31e4d9bfb5462c3f11311a
Tony Skinner <tonyskinner@masabi.com>
2019-02-27 16:12:24 +0000

    Getting the text back from the search results, now we need to do an assertion

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -1,8 +1,3 @@
-try:
-    from googlesearch import search
-except ImportError:
-    print("No module named 'google' found")
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -13,14 +8,32 @@ r = requests.get('https://pokeapi.co/api/v2/ability/51/')
 
 resultContent = json.loads(r.content)
 
+abilityName = resultContent['name']
+
 resultContent = resultContent['pokemon']
 
 lastResult = resultContent[len(resultContent) -1]
 
 pokemonName = lastResult['pokemon']['name']
 
-# for pokemonSearch in search(pokemonName, tld="co.uk",start=0,stop=10):
-#     print(pokemonSearch)
+print len(resultContent), "pokemon are able to use the", abilityName, "ability"
+print "We're now going to perform a google search on the last one in that list, which is the", pokemonName, "pokemon"
 
-driver = webdriver.chrome()
-driver.get("http.www.google.com")
+driver = webdriver.Chrome()
+driver.get("http://www.google.com")
+
+assert "Google" in driver.title
+elem = driver.find_element_by_css_selector("[name='q']")
+elem.clear()
+elem.send_keys(pokemonName)
+elem.send_keys(Keys.RETURN)
+
+elem = driver.find_elements_by_css_selector("div.r")
+
+for items in elem:
+    elem = items.text
+    print elem
+
+
+assert "No results found." not in driver.page_source
+driver.close()
--- seleniumTest.py
+++ seleniumTest.py
@@ -1,16 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# driver = webdriver.Chrome()
-# driver.get("http://www.google.com")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
-
 driver = webdriver.Chrome()
 driver.get("http://www.google.com")
 assert "Google" in driver.title
@@ -18,5 +8,5 @@ elem = driver.find_element_by_css_selector("[name='q']")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
+assert "No results found." not in driver.page_source
 driver.close()
