65c81d1ec018ba1c03d77f031fd5533423c51707
Tony Skinner <tonyskinner@masabi.com>
2019-02-27 12:51:29 +0000

    We have the results, now for doing the assertions

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -1,13 +1,7 @@
-# try:
-#     from googlesearch import search
-# except ImportError:
-#     print("No module named 'google' found")
-#
-# # to search
-# query = "something"
-#
-# for j in search(query, tld="co.uk",start=0,stop=10):
-#     print(j)
+try:
+    from googlesearch import search
+except ImportError:
+    print("No module named 'google' found")
 
 import requests
 import json
@@ -16,15 +10,11 @@ r = requests.get('https://pokeapi.co/api/v2/ability/51/')
 
 resultContent = json.loads(r.content)
 
-# resultContent = resultContent['pokemon']
-
 resultContent = resultContent['pokemon']
 
 lastResult = resultContent[len(resultContent) -1]
 
-print lastResult
+pokemonName = lastResult['pokemon']['name']
 
-print lastResult['pokemon']['name']
-
-# text = "bill,bob,ben"
-# print text.split(",")[1]
+for pokemonSearch in search(pokemonName, tld="co.uk",start=0,stop=10):
+    print(pokemonSearch)
